626364dad36148d256c530a0431e4151f1fa1cec
Lenin <lenin@softwarecriollo.com>
2014-04-25 15:41:35 -0430

    fix form, model and login

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,6 @@ class User < ActiveRecord::Base
   has_many :connectee, :class_name => "Connection", :foreign_key => "student_id"
 
   validates :email, uniqueness: true
-  validates :first_name,  presence: true
 
   def full_name
     return "#{first_name} #{last_name}".titleize
