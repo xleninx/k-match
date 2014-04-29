ef34219f50cc9f0d6b93603df9c115e678d17b83
Lenin <lenin@softwarecriollo.com>
2014-04-29 14:53:29 -0430

    Create dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,59 +3,95 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-  # link rel:"stylesheet",href:"//netdna.bootstrapcdn.com/bootswatch/3.1.1/flatly/bootstrap.min.css"
-  # link rel:"stylesheet",href:"//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css"
-  # script src:"//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"
     div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
+      # span class: "blank_slate" do
+      #   span I18n.t("active_admin.dashboard_welcome.welcome")
+      #   small I18n.t("active_admin.dashboard_welcome.call_to_action")
+      # end
     end
 
-    # div class: "container-fluid" do
-    #     div class: "col-md-4" do
-    #         div class: "well" do 
-    #             p class: "text-center", style:"font-size:85px" do
-    #                 "100"
-    #             end
-    #             p "recent"
-    #         end
-    #     end
-
-    #     div class: "col-md-4" do
-    #         div class: "well" do
-    #             p class: "text-center", style:"font-size:85px" 
-    #             p class: "text-center" 
-    #         end    
-    #     end    
-
-    #     div class: "col-md-4" do
-    #         div class: "well" do 
-    #             p class: "text-center", style:"font-size:85px" 
-    #             p class: "text-center" 
-    #         end    
-    #     end
-    # end            
-
-    # Here is an example of a simple dashboard with columns and panels.
-    
-    columns do
-      column do
-        panel "Recent Posts" do
-          ul do
-            User.all.map do |user|
-              li link_to(user.first_name, profile_path)
+    div class: "col-sm-4" do
+        div class: "well" do
+            span class: "title-module" do
+                User.where(:user_rights=> 3).count
             end
-          end
+            span class: "description-module" do
+                "Registered Prospective Students"
+            end
         end
-      end
-
-      column do
-        panel "Info" do
-          para "Welcome to ActiveAdmin."
-        end
-      end
     end
+
+    div class: "col-sm-4" do
+        div class: "well" do
+            span class: "title-module" do
+                "250"
+            end
+            span class: "description-module" do
+                "Conection Request"
+            end
+        end
+    end
+
+    div class: "col-sm-4" do
+        div class: "well" do
+            span class: "title-size" do "1" end 
+            span class: "description-size" do "hour" end
+            span class: "title-size" do "12" end
+            span class: "description-size" do "mins." end
+
+            span class: "description-module" do
+                "Average time for a completed connection request"
+            end
+        end
+    end
+
+    hr
+
+    h3 "Requests per program"
+
+    div class: "col-sm-12 text-center" do 
+        Program.all.each do |p|
+            div class: "col-sm-2" do
+                span class: "title-program" do p.users.count end
+
+                span class: "title-description" do "Program #{p.name}" end
+                hr style: "margin: 20px 35px; border: 1px solid \##{rand(0xffffff).to_s(16)};"
+            end
+        # div class: "col-sm-2" do
+        #     span class: "title-program" do "1" end
+
+        #     span class: "title-description" do "1" end
+        #     hr style: "margin: 20px 35px; border: 1px solid green;"
+        # end
+        # div class: "col-sm-2" do
+        #     span class: "title-program" do "1" end
+
+        #     span class: "title-description" do "1" end
+        #     hr style: "margin: 20px 35px; border: 1px solid green;"
+        # end
+        # div class: "col-sm-2" do
+        #     span class: "title-program" do "1" end
+
+        #     span class: "title-description" do "1" end
+        #     hr style: "margin: 20px 35px; border: 1px solid green;"
+        # end
+        # div class: "col-sm-2" do
+        #     span class: "title-program" do "1" end
+
+        #     span class: "title-description" do "1" end
+        #     hr style: "margin: 20px 35px; border: 1px solid green;"
+        # end
+        # div class: "col-sm-2" do
+        #     span class: "title-program" do "1" end
+
+        #     span class: "title-description" do "1" end
+        #     hr style: "margin: 20px 35px; border: 1px solid green;"
+        # end
+        end
+    end 
+
+
+
+
   end # content
 end
