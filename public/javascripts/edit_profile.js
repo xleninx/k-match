134c860ba41fdae99eefdb5703f13a92308fa4c0
Daniel Castillo <danielcfe@softwarecriollo.com>
2014-05-05 21:54:57 -0430

    Country - State, if user selects Country US, then State should be writable, if not then if should be greyed out.

--- public/javascripts/edit_profile.js
+++ public/javascripts/edit_profile.js
@@ -1,3 +1,25 @@
 $(function(){
   $('.chosen-select').chosen({max_selected_options: 3});
+
+  $('#state_select').on('change',function() {
+    country_id = $(this).val();
+    check_select( country_id );
+  });
+
+  $('#edit_profile').on('submit',function(){
+    $('.hidden').remove();
+  });
+
+  function check_select (country_id) {
+    if (country_id == 2067 || country_id == 2068){
+      $('#state').removeClass('hidden');
+      $('#state_text').addClass('hidden');
+    }else{
+      $('#state_text').removeClass('hidden');
+      $('#state').addClass('hidden');
+    }
+  }
+
+  check_select($('#state_select').val());
+
 })
