494adf6e76650e9f648791a4320d0a1b2e0091be
Daniel Castillo <danielcfe@softwarecriollo.com>
2014-05-16 17:04:24 -0430

    fix load helper

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,5 +1,7 @@
 class VideosController < ApplicationController
   skip_before_filter :authenticate_user!, :all
+  helper YoutubeHelper
+
   def index
     @main_video = Video.main_video
     @videos = Video.random_secondaries(3)
