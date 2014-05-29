6c4a3406283d9fc8a06ae7d896bd6f2b2a8f0d4a
Daniel Castillo <danielcfe@softwarecriollo.com>
2014-05-29 13:16:41 -0430

    show names of the student and prospective in connections xls file

--- app/admin/connection.rb
+++ app/admin/connection.rb
@@ -4,18 +4,38 @@ ActiveAdmin.register Connection do
   scope :joined, :default => true do |connection|
     connection.includes [:prospective_user,:current_user]
   end
+
+  fields_belong = { current_user: 'User',
+                    prospective_user: 'Prospective'
+                  }
+
+  fields_belong.each do |key, value|
+    config.xlsx_builder.column(value) do |resource|
+      source = resource.send(key.to_s)
+      (source)? source.full_name : ''
+    end
+  end
+
  index do
     selectable_column
     column "User" do |post|
       name = (post.current_user)? post.current_user.full_name : ''
       if post.current_user
-        link_to name, admin_user_path(post.current_user)
+        link_to name, (post.current_user.leader?)? admin_user_path(post.current_user) : admin_student_path(post.current_user)
       else
         ''
       end
     end
-    column "Prospective", :prospective_user
+    column "Prospective" do |post|
+      name = (post.prospective_user)? post.prospective_user.full_name : ''
+      if post.prospective_user
+        link_to name, admin_prospective_path(post.prospective_user)
+      else
+        ''
+      end
+    end
     column :message
+    column :status
     column :created_at
     column :updated_at
     actions
